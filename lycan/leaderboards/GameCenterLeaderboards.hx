c45693a97dfaeb6011e77267b2fcaa078981e959
sam <sam@silver42.karoo.co.uk>
2015-08-05 00:58:20 +0100

    Change gamecenter leaderboard backend to the gamecentermanager lib

--- lycan/leaderboards/GameCenterLeaderboards.hx
+++ lycan/leaderboards/GameCenterLeaderboards.hx
@@ -2,36 +2,38 @@ package lycan.leaderboards;
 
 #if gamecenterleaderboards
 
-import extension.gamecenter.GameCenter;
+import extension.gamecentermanager.GameCenterManager;
+import extension.gamecentermanager.GameCenterSortOrder;
 
 class GameCenterLeaderboards {
 	public static var get(default, never):GameCenterLeaderboards = new GameCenterLeaderboards();
 	
 	private function new() {
+		GameCenterManager.setupManager();
 	}
 	
 	public function openLeaderboard(id:String):Void {
-		GameCenter.showLeaderboard(id);
+		GameCenterManager.presentLeaderboards();
 	}
 	
 	public function openAchievements():Void {
-		GameCenter.showAchievements();
+		GameCenterManager.presentAchievements();
 	}
 	
 	public function isAvailable():Bool {
-		return GameCenter.available;
+		return GameCenterManager.isGameCenterAvailable();
 	}
 	
 	public function signIn():Void {
-		GameCenter.authenticate();
+		GameCenterManager.authenticateUser();
 	}
 	
-	public function submitScore(id:String, score:Int):Void {
-		GameCenter.reportScore(id, score);
+	public function submitScore(id:String, score:Int, sortOrder:GameCenterSortOrder = GameCenterSortOrder.HIGH_TO_LOW):Void {
+		GameCenterManager.saveAndReportScore(id, score, sortOrder);
 	}
 
-    public function updateAchievementProgress(id:String, percent:Float, showBanner:Bool=true):Void {
-		GameCenter.reportAchievement(id, percent, showBanner);
+    public function updateAchievementProgress(id:String, percent:Float, showBanner:Bool = true):Void {
+		GameCenterManager.saveAndReportAchievement(id, percent, showBanner);
     }
 }
 
