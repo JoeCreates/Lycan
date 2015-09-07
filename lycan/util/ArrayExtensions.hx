b90fd22fa89fd1f42639a711d2cea0e3e91a3cec
sam <sam@silver42.karoo.co.uk>
2015-09-07 03:26:20 +0100

    More array extensions

--- lycan/util/ArrayExtensions.hx
+++ lycan/util/ArrayExtensions.hx
@@ -4,7 +4,32 @@ package lycan.util;
 
 // TODO see thx.core implementations
 class ArrayExtensions {
+	public static function randomElementFromArrays<T>(arrays:Array<Array<T>>):Null<T> {
+		Sure.sure(arrays != null && arrays.length != 0);
+		var totalLength:Int = 0;
+		var lengths = [];
+		
+		for (array in arrays) {
+			Sure.sure(array != null && array.length != 0);
+			totalLength += array.length;
+			lengths.push(totalLength);
+		}
+		
+		var n:Float = Math.random() * totalLength;
+		
+		var i = 0;
+		while (i < lengths.length) {
+			if (n < lengths[i]) {
+				return randomElement(arrays[i]);
+			}
+			i++;
+		}
+		
+		throw "Failed to get random element";
+	}
+	
 	inline public static function randomElement<T>(array:Array<T>):Null<T> {
+		Sure.sure(array != null && array.length != 0);
 		return array[Std.random(array.length)];
 	}
 }
