025a92e25e1c04766f462752ec00b7c07f8fbc99
sam <sam@silver42.karoo.co.uk>
2015-09-09 20:08:03 +0100

    Make distance metrics use floats not flxpoints

--- lycan/util/DistanceMetrics.hx
+++ lycan/util/DistanceMetrics.hx
@@ -3,11 +3,11 @@ package lycan.util;
 import flixel.math.FlxPoint;
 
 class DistanceMetrics {
-	public static inline function manhattan(p1:FlxPoint, p2:FlxPoint):Float {
-		return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
+	public static inline function manhattan(x1:Float, y1:Float, x2:Float, y2:Float):Float {
+		return Math.abs(x1 - x2) + Math.abs(y1 - y2);
 	}
 	
-	public static inline function chebyshev(p1:FlxPoint, p2:FlxPoint):Float {
-		return Math.max(Math.abs(p1.x - p2.x), Math.abs(p1.y - p2.y));
+	public static inline function chebyshev(x1:Float, y1:Float, x2:Float, y2:Float):Float {
+		return Math.max(Math.abs(x1 - x2), Math.abs(y1 - y2));
 	}
 }
