90638d0b15dca5541fb1e5e4967a48414c6f8685
sam <sam@silver42.karoo.co.uk>
2015-09-17 13:51:03 +0100

    Euclidean distance metrics

--- lycan/util/DistanceMetrics.hx
+++ lycan/util/DistanceMetrics.hx
@@ -10,4 +10,12 @@ class DistanceMetrics {
 	public static inline function chebyshev(x1:Float, y1:Float, x2:Float, y2:Float):Float {
 		return Math.max(Math.abs(x1 - x2), Math.abs(y1 - y2));
 	}
+	
+	public static inline function euclidean(x1:Float, y1:Float, x2:Float, y2:Float):Float {
+		return Math.sqrt(euclideanSquared(x1, y1, x2, y2));
+	}
+	
+	public static inline function euclideanSquared(x1:Float, y1:Float, x2:Float, y2:Float):Float {
+		return Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2);
+	}
 }
