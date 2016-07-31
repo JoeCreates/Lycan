42e43a218f016243da2674c57da33544fdbff4fc
sam <sam@silver42.karoo.co.uk>
2016-07-31 20:15:33 +0100

    Require substate to be explicitly specified in ConditionalEvent (the FlxG approach didn't work in werewolf tycoon 1)

--- lycan/util/ConditionalEvent.hx
+++ lycan/util/ConditionalEvent.hx
@@ -3,6 +3,7 @@ package lycan.util;
 import flixel.FlxBasic;
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.FlxSubState;
 
 class ConditionalEvent extends FlxBasic {
 	public var condition:Void->Bool;
@@ -10,17 +11,17 @@ class ConditionalEvent extends FlxBasic {
 	public var state:FlxState;
 	
 	/** Begin waiting for a condition to be fulfilled before calling a callback */
-	public static function wait(condition:Void->Bool, callback:ConditionalEvent->Void):ConditionalEvent {
-		var cond:ConditionalEvent = new ConditionalEvent(condition, callback);
+	public static function wait(state:FlxSubState, condition:Void->Bool, callback:ConditionalEvent->Void):ConditionalEvent {
+		var cond:ConditionalEvent = new ConditionalEvent(condition, callback, state);
 		cond.state.add(cond);
 		return cond;
 	}
 	
-	private function new(condition:Void->Bool, callback:ConditionalEvent->Void) {
+	private function new(condition:Void->Bool, callback:ConditionalEvent->Void, state:FlxSubState) {
 		super();
 		this.condition = condition;
 		this.callback = callback;
-		this.state = FlxG.state.subState;
+		this.state = state;
 	}
 	
 	override public function update(dt:Float):Void {
