5c0ca94f94fae3c5417eca5069483043d05b7646
Joe Williamson <huruey@gmail.com>
2018-02-11 16:42:38 +0000

    TODO put lerpBounds into flixel

--- lycan/util/MathUtil.hx
+++ lycan/util/MathUtil.hx
@@ -6,6 +6,7 @@ import flixel.math.FlxMath;
 import flixel.util.helpers.FlxBounds;
 
 class MathUtil {
+	// TODO just get this in flixel
 	public static function lerpBounds(bounds:FlxBounds<Float>, t:Float):Float {
 		return FlxMath.lerp(bounds.min, bounds.max, t);
 	}
